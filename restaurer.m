406af50541658b08e6f12d3a7e75b8977b5a57b1
Sn0wFox <ruben.pericas@laposte.net>
2015-11-19 16:39:31 +0100

    Restauration OK
    On peut désormais restaurer une image.
    Concernant les fftshift, il faut qu'ils soient ou partout, ou null part
    sauf pour affichage. Pour le moment ils sont partout, puisqu'ils m'ont
    servi a debugguer, et c'est très bien comme ça.

--- restaurer.m
+++ restaurer.m
@@ -1,41 +1,70 @@
 function [ restoredImg ] = restaurer( image, imgRef )
 
-% L'image dégradée
-D = fft2(image);
+%map niveau de gris
+mapndg = ([0:255]/255)'*[1 1 1];
 
-% La modèlisation du filtre de dégradation (spatial)
+
+
+% La modelisation du filtre de degradation (spatial)
 h = zeros(size(image));
 sizeImgX = size(image,1);
 sizeImgY = size(image,2);
 sizeX = 5;
-sizeY = 3;
-for i=floor(sizeImgX/2) - floor(sizeX/2):floor(sizeImgX/2) + floor(sizeX/2)
-    for j=floor(sizeImgY/2) - floor(sizeY/2):floor(sizeImgY/2) + floor(sizeY/2)
+sizeY = 7;
+for i=1+floor(sizeImgX/2) - floor(sizeX/2):1+floor(sizeImgX/2) + floor(sizeX/2)
+    for j=1+floor(sizeImgY/2) - floor(sizeY/2):1+floor(sizeImgY/2) + floor(sizeY/2)
         h(i,j) = 1/(sizeX*sizeY);
     end
 end
 
-% La modèlisation du filtre de dégradation (fréquentiel)
-H = fft2(h);
+% La modelisation du filtre de degradation (frequentiel)
+H = fftshift(fft2(h));
 
-% Le spectre de puissance de l'image idéale
-modI = abs(fft2(imgRef));
+% Le spectre de puissance de l'image ideale
+modI = abs(fftshift(fft2(imgRef)));
 Pi = modI.*modI;
 
+
+
 % Le spectre de puissance du bruit
-Dref = fft2(imgRef).*H;
-dref=ifft2(Dref);
-drefInt = round(dref);
+Dref = fftshift(fft2(imgRef)).*H;
+dref=fftshift(ifft2(Dref));
+
+
+
+drefInt = floor(dref);
 b = dref - drefInt;
-modB = abs(fft2(b));
+
+modB = abs(fftshift(fft2(b)));
 Pb = modB.*modB;
+
+
 
 % Filtre de restauration de Wiener
 absH2 = abs(H).*abs(H);
 W = (1./H).*(absH2./(absH2+Pb./Pi));
 
-% Restauration
-restoredImg = D.*W;
 
+
+% L'image degradee
+D = fftshift(fft2(image));
+
+% Restauration : spectre
+RestoredImg = D.*W;
+
+% Transformee inverse
+restoredImg = fftshift(ifft2(RestoredImg));
+
+AfficherImg(logNormalise(restoredImg), mapndg);
+return;
+
+end
+
+
+function [affIM] = logNormalise(img)
+    affIM = abs(img)+1;
+    maxi = max(max(affIM));
+    mini = min(min(affIM));
+    affIM = (log(affIM)-log(mini))/(log(maxi)-log(mini))*255;
 end
 
